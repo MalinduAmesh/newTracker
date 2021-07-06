2df5b508cd8511466fe96ca09d199fa4b90f52a7
MalinduAmesh <ameshmalindu006@gmail.com>
2021-07-07 00:44:46 +0530

    ss

--- index.js
+++ index.js
@@ -2,25 +2,10 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
 const dbConfig = require('./config/db.config')
-const cors = require('cors');
 const bodyPharse = require('body-parser')
 const requireToken = require('./middleware/requireToken'); 
 
-var corsOptions = {
-    origin: "http://localhost:8081"
-  };
-  
-  app.use(cors(corsOptions));
 
-
-
-// mongoose.connect('mongodb://localhost/new')
-// .then(function (){
-//     console.log('connected to The Database')
-// })
-// .catch(function(){
-//     console.log('Could not connected to the DB')
-// })
 
 mongoose
   .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
